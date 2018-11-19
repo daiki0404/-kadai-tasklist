d8b3569893dd3d11931aa97ef3eded8e97c0cbc1
EC2 Default User <d.h.19890404@gmail.com>
2018-11-19 11:58:48 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "タスクが作成されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクの作成に失敗しました。"
+      render :new
+    end
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "更新しました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "更新できませんでした"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @message = find(params[:id])
+    @message.destroy
+    flash[:success] = "正常に削除されました"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
